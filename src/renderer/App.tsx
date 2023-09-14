6d3b2af19e18e5ed522bceaf4eba14ae5dc8c164
haouarihk <haithem2001@gmail.com>
2023-09-14 08:21:35 +0100

    design update

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -58,61 +58,82 @@ function LocalEmbeddingAPI() {
     handleStartServer();
   }, []);
 
+  useEffect(() => {
+    document.title = `${document.title.split('-')[0]}-${serverStatus}`;
+  }, [serverStatus]);
+
   const basePath = `http://localhost:${serverPort || 'PORT'}/api`;
 
   return (
     <div className="App bg-gray-100 dark:bg-black select-none flex flex-col md:flex-row w-screen h-screen overflow-hidden">
-      <div className="w-full md:max-w-xs">
-        <div className="flex p-3 gap-4 flex-col">
-          <div className="stat-title">API Server</div>
+      <div className="w-full h-full md:max-w-xs flex flex-col justify-between">
+        <div className="flex p-2 px-10 gap-4 flex-col">
+          <div className="stat-title p-2">
+            API Server (
+            <span
+              className={clsx('status-text min-w-max', {
+                'text-green-300': serverStatus === 'Running',
+                'text-red-300': serverStatus === 'Stopped',
+              })}
+            >
+              {serverStatus}
+            </span>
+            )
+          </div>
           <div className="stat-value">{serverStatus}</div>
-          <div className="flex items-end flex-wrap md:items-start justify-between gap-3 md:flex-col w-full">
-            <div className="form-control">
-              <span>Basepath</span>
-              <label className="input-group">
-                <input
-                  type="text"
-                  className="input input-sm select-all input-bordered"
-                  value={basePath}
-                  readOnly
-                  onChange={handlePortChange}
-                />
-              </label>
-            </div>
-            <div className="form-control">
+          <div className="flex items-end flex-wrap md:items-start justify-between gap-3 sm:flex-col w-full">
+            <div className="form-control w-full xs:w-auto sm:w-full">
               <span>Port</span>
-              <label className="input-group">
+              <label className="input-group w-full">
                 <input
                   type="text"
                   placeholder="3005"
-                  className="input input-sm input-bordered"
+                  className="input input-sm w-full xs:w-auto sm:w-full input-bordered"
                   value={port}
                   onChange={handlePortChange}
                 />
               </label>
             </div>
-            {serverStatus === 'Running' ? (
-              <button
-                className="btn btn-sm btn-error"
-                onClick={handleStopServer}
-              >
-                Stop Server
-              </button>
-            ) : (
-              <button
-                className="btn btn-sm btn-success"
-                onClick={handleStartServer}
-              >
-                Start Server
-              </button>
-            )}
+            <div className="flex justify-center items-center w-full xs:w-auto sm:w-full py-5">
+              {serverStatus === 'Running' ? (
+                <button
+                  className="btn w-full btn-md btn-error"
+                  onClick={handleStopServer}
+                >
+                  Stop Server
+                </button>
+              ) : (
+                <button
+                  className="btn w-full btn-md btn-success"
+                  onClick={handleStartServer}
+                >
+                  Start Server
+                </button>
+              )}
+            </div>
           </div>
         </div>
+        <div className="p-4 px-10 flex flex-col">
+          <div className="form-control w-full xs:w-auto sm:w-full">
+            <span>Basepath</span>
+            <label className="input-group w-full">
+              <input
+                type="text"
+                className="input input-ghost input-sm select-all w-full xs:w-auto sm:w-full input-bordered"
+                value={basePath}
+                readOnly
+                onChange={handlePortChange}
+              />
+            </label>
+          </div>
+        </div>{' '}
       </div>
-      <div className="w-full bg-white dark:bg-gray-900 overflow-y-auto">
-        <div className="p-2 flex flex-col gap-2 w-full">
-          <div className="w-full text-center text-lg font-bold">Services</div>
-          <div className="p-3 border-black rounded-md flex flex-col gap-2">
+      <div className="w-full bg-white dark:bg-gray-900 overflow-hidden">
+        <div className="flex flex-col gap-2 w-full h-full">
+          <div className="w-full p-3 pl-10 text-lg font-bold bg-gray-100 dark:bg-black">
+            <div className="stat-title">Services</div>
+          </div>
+          <div className="p-3 border-black rounded-md h-full flex flex-col gap-4 overflow-y-auto">
             {availableServices.map((service) => (
               <ServiceItem key={service} name={service} url={basePath} />
             ))}
@@ -171,60 +192,63 @@ function ServiceItem(props: { name: string; url: string }) {
   }, [props.name]);
 
   return (
-    <div className="card w-full shadow-xl dark:bg-black">
-      <div className="card-body">
-        <div className="w-full flex justify-between items-center">
-          <h2 className="card-title w-full">{props.name}</h2>
-          <p
-            className={clsx('status-text min-w-max', {
-              'text-green-300': status?.status === 'ready',
-            })}
-          >
-            {status?.status === 'progress' ? (
-              <>progress ({status?.progress.toFixed(2)}%)</>
-            ) : (
-              status?.status || 'Not Ready'
-            )}
-          </p>
-        </div>
-        <div>
-          <p className="status-text">Repo: {status?.name}</p>
-          {status?.total && (
-            <p className="status-text">
-              Size: {(status.total * 10 ** -6).toFixed(2)}MB
-            </p>
-          )}
-        </div>
-
-        <div className="card-actions w-full items-center justify-between">
-          <div className="flex items-center">
-            <label>Model:</label>
-            <select
-              className="select w-full select-ghost"
-              value={selectedModel}
-              onChange={(e) => {
-                setSelectedModel(e.target.value);
-              }}
+    <div className="card w-full shadow-xl dark:shadow-white/5 dark:bg-black">
+      <div className="card-body flex flex-col gap-8">
+        <div className="flex flex-col w-full gap-2">
+          <div className="w-full flex justify-between items-center">
+            <h2 className="card-title w-full">{props.name}</h2>
+            <p
+              className={clsx('status-text min-w-max', {
+                'text-green-300': status?.status === 'ready',
+              })}
             >
-              {models?.map((model) => (
-                <option key={model.name} value={model.name}>
-                  {model.name} {model.loaded ? '(Loaded)' : ''}
-                </option>
-              ))}
-            </select>
+              {status?.status === 'progress' ? (
+                <>progress ({status?.progress.toFixed(2)}%)</>
+              ) : (
+                status?.status || 'Not Ready'
+              )}
+            </p>
           </div>
-          <button
-            className={clsx('btn btn-sm', {
-              disabled: status?.loaded && status.name === selectedModel,
-            })}
-            disabled={!!status?.loaded && status.name === selectedModel}
-            onClick={handleLoadModel}
-          >
-            Load Model
-          </button>
+          <div>
+            <p className="status-text">Repo: {status?.name}</p>
+            {status?.total && (
+              <p className="status-text">
+                Size: {(status.total * 10 ** -6).toFixed(2)}MB
+              </p>
+            )}
+          </div>
+        </div>
+        <ServiceExamples examples={info.examples} url={props.url} />
+        <div className="flex flex-col card-actions w-full">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center">
+              <label>Model:</label>
+              <select
+                className="select w-full select-ghost"
+                value={selectedModel}
+                onChange={(e) => {
+                  setSelectedModel(e.target.value);
+                }}
+              >
+                {models?.map((model) => (
+                  <option key={model.name} value={model.name}>
+                    {model.name} {model.loaded ? '(Loaded)' : ''}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button
+              className={clsx('btn btn-sm', {
+                disabled: status?.loaded && status.name === selectedModel,
+              })}
+              disabled={!!status?.loaded && status.name === selectedModel}
+              onClick={handleLoadModel}
+            >
+              Load Model
+            </button>
+          </div>
         </div>
       </div>
-      <ServiceExamples examples={info.examples} url={props.url} />
     </div>
   );
 }
--- src/renderer/components/service/examples.tsx
+++ src/renderer/components/service/examples.tsx
@@ -8,7 +8,7 @@ export default function ServiceExamples(props: {
   url: string;
 }) {
   return (
-    <div className="flex flex-col gap-3 p-4">
+    <div className="flex flex-col w-full gap-3">
       <div className="flex flex-col gap-2">
         {props.examples?.map((example) => (
           <div key={example.curl} className="mockup-code">
